bf8b013c84b75343908e92af4b9c7df6d4106cb9
Albert <albert.gee.ca@gmail.com>
2023-11-23 15:40:31 -0800

    Refactor

--- src/ca/bcit/comp7005/Main.java
+++ src/ca/bcit/comp7005/Main.java
@@ -1,21 +1,18 @@
 package ca.bcit.comp7005;
 
-import java.util.Scanner;
-
 public class Main {
 
-    private static final int PACKET_BUFFER_SIZE = 65535;
-
     public static void main(String[] args) {
+        try {
+            int port = Integer.parseInt(args[0]);
+            Receiver receiver = new Receiver(port);
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the port number: ");
-        int port = scanner.nextInt();
+            System.out.println("Receiver started on port " + port);
+            System.out.println("Waiting for a connection...");
 
-        Receiver receiver = new Receiver(port, PACKET_BUFFER_SIZE);
-        receiver.listen();
-
-
+            receiver.listen();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-
 }
--- src/ca/bcit/comp7005/Receiver.java
+++ src/ca/bcit/comp7005/Receiver.java
@@ -3,54 +3,128 @@ package ca.bcit.comp7005;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
+import java.security.NoSuchAlgorithmException;
 
 public class Receiver {
 
-    private final int port;
-    private final int packetBufferSize;
+    private final DatagramSocket socket;
 
-    public Receiver(int port, int packetBufferSize) {
-        System.out.println("Receiver started on port " + port);
+    private int sequenceNumber;
+    private int senderSequenceNumber;
 
-        this.port = port;
-        this.packetBufferSize = packetBufferSize;
+    private InetAddress senderAddress;
+
+    private int senderPort;
+
+    public Receiver(int port) throws SocketException {
+        this.socket = new DatagramSocket(port);
+        printSocketDetails();
     }
 
-    public void listen() {
+    public void listen() throws IOException, NoSuchAlgorithmException {
+        acceptConnection();
 
-        try(DatagramSocket socket = new DatagramSocket(port)) {
-            printSocketDetails(socket);
+        processReceivingData();
 
-            processReceivingData(socket);
+        disconnectFromSender();
+    }
 
-        } catch(Exception e) {
-            System.out.println("Error: " + e.getMessage());
+    public void acceptConnection() throws IOException, NoSuchAlgorithmException {
+        // receive syn
+        DatagramPacket packet = receiveDatagramPacket();
+        AZRP synAzrp = AZRP.fromBytes(packet.getData());
+
+        if (!synAzrp.isSYN()) {
+            throw new RuntimeException("The received packet is not a SYN packet.");
+        } else {
+            this.senderSequenceNumber = synAzrp.getSequenceNumber();
+            this.sequenceNumber = AZRP.generateInitialSequenceNumber();
+            this.senderAddress = packet.getAddress();
+            this.senderPort = packet.getPort();
+
+            System.out.println("Received SYN AZRP: " + synAzrp);
         }
+
+        // send syn ack
+        senderSequenceNumber++;
+        AZRP aknowledgementAzrp = AZRP.synAck(sequenceNumber, senderSequenceNumber);
+        aknowledgementAzrp.send(socket, senderAddress, senderPort);
+        System.out.println("Sent SYN-ACK: " + aknowledgementAzrp);
+
+        // receive ack
+        DatagramPacket ackPacket = receiveDatagramPacket();
+        AZRP ackAzrp = AZRP.fromBytes(ackPacket.getData());
+        if (!ackAzrp.isACK()) {
+            throw new RuntimeException("The received packet is not a ACK packet.");
+        }
+        System.out.println("Received ACK AZRP: " + ackAzrp);
     }
 
-    private void processReceivingData(DatagramSocket socket) throws IOException {
-        byte[] packetBuffer = new byte[packetBufferSize];
+    private void disconnectFromSender() throws IOException {
+        // Send a FIN packet to the sender
+        AZRP finAzrp = AZRP.fin(sequenceNumber, 0);
+        finAzrp.send(socket, senderAddress, senderPort);
+
+        // Wait for the sender to acknowledge the FIN packet
+        DatagramPacket finAckPacket = receiveDatagramPacket();
+        AZRP finAckAzrp = AZRP.fromBytes(finAckPacket.getData());
+
+        if (!finAckAzrp.isACK()) {
+            throw new RuntimeException("The received packet is not a ACK packet.");
+        }
+        System.out.println("Received FIN-ACK AZRP: " + finAckAzrp);
+
+        // Close the socket
+        socket.close();
+
+        this.sequenceNumber = 0;
+        this.senderSequenceNumber = 0;
+        this.senderAddress = null;
+        this.senderPort = 0;
+
+        System.out.println("Disconnected from sender.");
+    }
+
+
+
+    private DatagramPacket receiveDatagramPacket() throws IOException {
+        byte[] packetBuffer = new byte[AZRP.MAXIMUM_PACKET_SIZE_IN_BYTES];
+        DatagramPacket packet = new DatagramPacket(packetBuffer, packetBuffer.length);
+        socket.receive(packet);
+        return packet;
+    }
+
+
+    private void processReceivingData() throws IOException {
 
         boolean isRunning = true;
         while(isRunning) {
-            System.out.println("\nWaiting for a message...");
+            DatagramPacket packet = receiveDatagramPacket();
+            AZRP azrp = AZRP.fromBytes(packet.getData());
 
-            DatagramPacket packet = new DatagramPacket(packetBuffer, packetBuffer.length);
-            socket.receive(packet);
-            String message = new String(packet.getData(), 0, packet.getLength());
-
-            if (message.equals("exit")) {
+            if (azrp.isFIN()) {
+                System.out.println("Received FIN AZRP: " + azrp);
                 isRunning = false;
-                continue;
-            }
 
-            System.out.println("Received: " + message);
-            packetBuffer = new byte[packetBufferSize];
+                this.senderSequenceNumber = azrp.getSequenceNumber() + 1;
+                AZRP ackAzrp = AZRP.ack(this.sequenceNumber, this.senderSequenceNumber);
+                ackAzrp.send(socket, senderAddress, senderPort);
+
+            } else {
+                System.out.println("Received: (" + azrp.validateChecksum() + "): " + azrp);
+
+                this.sequenceNumber++;
+                this.senderSequenceNumber = azrp.getSequenceNumber() + azrp.getData().length;
+                AZRP ackAzrp = AZRP.ack(this.sequenceNumber, this.senderSequenceNumber);
+                ackAzrp.send(socket, senderAddress, senderPort);
+            }
         }
     }
 
-    private void printSocketDetails(DatagramSocket socket) throws SocketException {
+
+    private void printSocketDetails() throws SocketException {
         System.out.println("- Local Port: " + socket.getLocalPort());
         System.out.println("- Timeout: " + socket.getSoTimeout());
         System.out.println("- Is bound: " + socket.isBound());
